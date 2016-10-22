f47cec5ab264568cd15a0dcdc7f00f3f83b94b6f
Helder De Oliveira <hemarque@gmail.com>
2016-10-22 10:18:27 +0200

    count neighbours

--- src/main/java/org/gdcr/gameOfLife/GameOfLife.java
+++ src/main/java/org/gdcr/gameOfLife/GameOfLife.java
@@ -1,18 +1,59 @@
 package org.gdcr.gameOfLife;
 
 public class GameOfLife {
-	String board = "...\n...\n...\n";
+
+	private final int rows;
+	private final int columns;
+	private final String[][] board;
 
 	public GameOfLife(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+		board = new String[rows][columns];
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				board[i][j] = ".";
+			}
+		}
 	}
 
 	@Override
 	public String toString() {
-		return board;
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				sb.append(board[i][j]);
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
 	}
 
 	public void addCell(int row, int col) {
-		board = "O..\n...\n...\n";
+		if (row >= 0 && row < this.rows && col >= 0
+				&& col < this.columns)
+			board[row][col] = "O";
+	}
+
+	public int countNeighbours(int row, int col) {
+		int neighbours = 0;
+		if (board[row - 1][col - 1].equals("O"))
+			neighbours++;
+		if (board[row - 1][col].equals("O"))
+			neighbours++;
+		if (board[row - 1][col + 1].equals("O"))
+			neighbours++;
+		if (board[row][col - 1].equals("O"))
+			neighbours++;
+		if (board[row][col + 1].equals("O"))
+			neighbours++;
+		if (board[row + 1][col - 1].equals("O"))
+			neighbours++;
+		if (board[row + 1][col].equals("O"))
+			neighbours++;
+		if (board[row + 1][col + 1].equals("O"))
+			neighbours++;
+		return neighbours;
 	}
 
 }
